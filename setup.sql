0d7a09aa1a468ad5035825850148b5aec753999b
FreeSoul <58299651+free-soul@users.noreply.github.com>
2022-07-12 17:08:07 +0900

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -2,9 +2,9 @@ CREATE SCHEMA `note` DEFAULT CHARACTER SET utf8 ;
 
 CREATE TABLE `note`.`boards` (
   `creator` VARCHAR(45) NOT NULL,
-  `createdAt` VARCHAR(45) NULL DEFAULT NULL,,
+  `createdAt` DATETIME NULL DEFAULT NULL,,
   'title' VARCHAR(45) NULL,
-  `content` VARCHAR(45) NULL,
+  `content` LONGTEXT NULL,
   PRIMARY KEY (`creator`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
